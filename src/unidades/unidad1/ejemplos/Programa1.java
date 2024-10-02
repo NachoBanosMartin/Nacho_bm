a5c0b52f7ed41406b699ebf553a92e67f752241a
diurno <diurno@equipo-00>
2024-10-02 13:32:40 +0200

    Cambios

--- src/unidades/unidad1/ejemplos/Programa1.java
+++ src/unidades/unidad1/ejemplos/Programa1.java
@@ -3,7 +3,7 @@ package unidades.unidad1.ejemplos;
 public class Programa1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Primer Proyecto");
 
 	}
 
