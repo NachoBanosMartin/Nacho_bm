2eb7d5a0378ec90ac8fc0fc66590e5cafb4935c9
nacho <nacho@LAPTOP-8ESOIKJT>
2024-10-16 16:58:56 +0200

    Ejercicio Tabla 16 de Octubre

--- src/unidades/unidad2/ejercicios/EjercicioClase.java
+++ src/unidades/unidad2/ejercicios/EjercicioClase.java
@@ -5,21 +5,21 @@ public class EjercicioClase {
 	public static void main(String[] args) {	
 		
 	
-    	final String col_DNI = "DNI  |";
-    	final String col_NOMBRE = "NOMBRE |";
-    	final String col_DIRECCIÓN = "DIRECCIÓN|";
-    	final String col_CC = "CC            |";
-    	final String col_PASTA = "PASTA        |";
+    	final String col_DNI = "DNI            |";
+    	final String col_NOMBRE = "NOMBRE   |";
+    	final String col_DIRECCIÓN = "DIRECCIÓN     |";
+    	final String col_CC = "CUENTA       |";
+    	final String col_PASTA = "DINERO       |";
 
     	
-    	System.out.println("      ________________________________________________________________________");
-    	System.out.println("      |____________________________DATOS CLIENTE_____________________________|");
+    	System.out.println("       ______________________________________________________________________________________");
+    	System.out.println("      |____________________________________DATOS CLIENTE_____________________________________|");
         System.out.printf("\t%s\t%s\t%s\t%s\t%s\n", col_DNI,col_NOMBRE,col_DIRECCIÓN ,col_CC,col_PASTA);
-    	System.out.println("      |----------------|-----------------|--------------------|--------------|");
-        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","93939|", "LUIS   |", "99494943 |", "c/SALSIPUEDES |", "983483949848 |", "934.56€ |");
-    	System.out.println("      |----------------|-----------------|--------------------|--------------|");
-        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","11122|", "DANIEL |", "93939394 |", "c/NOVENGAS    |", "949494949494 |", "-12.45€  |");
-    	System.out.println("      |________________|_________________|____________________|______________|");
+    	System.out.println("      |----------------|-----------------|--------------------|--------------|---------------|");
+        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","28472413       |", "Octavio  |", "636728933     |", "C/Pichón     |", "234987345981 |", "9060€ |");
+    	System.out.println("      |----------------|-----------------|--------------------|--------------|---------------|");
+        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","87432519       |", "Ana      |", "987345923     |", "C/Alcaparra  |", "873459271892 |", "23,5€  |");
+    	System.out.println("      |________________|_________________|____________________|______________|_______________|");
     	
     	
 	}
