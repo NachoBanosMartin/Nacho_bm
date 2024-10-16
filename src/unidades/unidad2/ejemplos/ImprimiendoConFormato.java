670ae4d302280a6271fdefcc14786f1dc3e96185
diurno <diurno@equipo-00>
2024-10-16 13:44:24 +0200

    Clase 16 de oct

--- src/unidades/unidad2/ejemplos/ImprimiendoConFormato.java
+++ src/unidades/unidad2/ejemplos/ImprimiendoConFormato.java
@@ -12,7 +12,11 @@ public class ImprimiendoConFormato {
 		System.out.printf("Total es: %,.2f $%n", cantidad);
 		System.out.printf("Total es: %-10.2f", cantidad);
 		
-		
+		int numeroBinario = 0b100101;
+		System.out.printf("El numero en decimal es: %d%n", numeroBinario);
+		System.out.printf("El numero en decimal es: %s%n", Integer.toBinaryString(valorEntero));
+		double nd = 123.94536;
+		System.out.printf("\t%.3f%n\t%.3e%n\t%.3g%n", nd, nd, nd);
 	}
-
 }
+
---
+++ src/unidades/unidad2/ejercicios/EjercicioClase.java
@@ -0,0 +1,26 @@
+package unidades.unidad2.ejercicios;
+
+public class EjercicioClase {
+
+	public static void main(String[] args) {	
+		
+	
+    	final String col_DNI = "DNI  |";
+    	final String col_NOMBRE = "NOMBRE |";
+    	final String col_DIRECCIÓN = "DIRECCIÓN|";
+    	final String col_CC = "CC            |";
+    	final String col_PASTA = "PASTA        |";
+
+    	
+    	System.out.println("      ________________________________________________________________________");
+    	System.out.println("      |____________________________DATOS CLIENTE_____________________________|");
+        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n", col_DNI,col_NOMBRE,col_DIRECCIÓN ,col_CC,col_PASTA);
+    	System.out.println("      |----------------|-----------------|--------------------|--------------|");
+        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","93939|", "LUIS   |", "99494943 |", "c/SALSIPUEDES |", "983483949848 |", "934.56€ |");
+    	System.out.println("      |----------------|-----------------|--------------------|--------------|");
+        System.out.printf("\t%s\t%s\t%s\t%s\t%s\n","11122|", "DANIEL |", "93939394 |", "c/NOVENGAS    |", "949494949494 |", "-12.45€  |");
+    	System.out.println("      |________________|_________________|____________________|______________|");
+    	
+    	
+	}
+}
