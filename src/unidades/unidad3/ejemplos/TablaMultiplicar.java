8e8ebeabf43117038a49b8ae5fda8cb441e069df
diurno <diurno@equipo-00>
2024-10-30 13:10:29 +0100

    Clase 30 octubre

---
+++ src/unidades/unidad3/ejemplos/TablaMultiplicar.java
@@ -0,0 +1,30 @@
+package unidades.unidad3.ejemplos;
+
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner entrada = new Scanner (System.in);
+		System.out.printf("Introduzca un número (1-9)");
+		int n = entrada .nextInt();
+
+		
+		//Recorrer las filas
+		for (int fila = 1; fila <= n; fila ++ ) {
+			
+			//Recorrer las columnas
+			for (int columna = 1; columna <= 10; columna ++) {
+		
+				System.out.printf("%6d", fila * columna);
+			}
+			
+			System.out.println();
+		
+		}
+		
+		entrada.close();
+	}
+}
