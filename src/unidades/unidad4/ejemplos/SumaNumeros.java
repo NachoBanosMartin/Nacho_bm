63f2a3f2ed9e3b1c44900705b9ea7983c1ed277d
diurno <diurno@equipo-00>
2024-11-06 13:35:59 +0100

    Clase 6 de Noviembre

---
+++ src/unidades/unidad4/ejemplos/SumaNumeros.java
@@ -0,0 +1,52 @@
+package unidades.unidad4.ejemplos;
+
+import java.util.Scanner;
+
+public class SumaNumeros {
+
+	static  int NUM_VECES = 3;
+	static Scanner scanner;
+	public static void main(String[] args) {
+		
+	     	scanner = new Scanner(System.in);
+	       	realizarSumas();
+	       	scanner.close();
+	}
+	static void realizarSumas() {
+		
+		for (int i = 1; i <= NUM_VECES; i ++) {
+	        
+	        mostrarMensaje("Intento "+i+" - Ingresa el primer número: ");
+	        int num1 = leerEntero();
+	        mostrarMensaje("Intento "+i+" - Ingresa el segundo número: ");
+	        int num2 = leerEntero();
+	        int suma1 = sumar(num1,num2);
+	        System.out.println("La suma es: " + suma1);
+	        
+	       	}
+      
+	}
+	
+	
+	static int sumar(int sumando1, int sumando2) {
+		
+		int resultado = sumando1 + sumando2;		
+		return resultado;
+	}
+	
+	static int leerEntero() {
+		
+		Scanner scanner = new Scanner(System.in);		
+		int numero = scanner.nextInt();		
+		return numero;
+				
+	}
+	
+	static void mostrarMensaje (String mensaje) {
+		
+		System.out.println(mensaje);
+	}
+	
+	
+
+}
