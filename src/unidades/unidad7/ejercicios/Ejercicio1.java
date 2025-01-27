7dc649e3b167ebc5565175023b206eba3f434e74
diurno <diurno@192.168.30.20>
2025-01-27 10:46:31 +0100

    vamos

--- src/unidades/unidad7/ejercicios/Ejercicio1.java
+++ src/unidades/unidad7/ejercicios/Ejercicio1.java
@@ -1,9 +1,11 @@
 package unidades.unidad7.ejercicios;
 
 public class Ejercicio1 {
+	
+	// Escriba un programa Java generar un número aleatorio entre 1 y 10.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
