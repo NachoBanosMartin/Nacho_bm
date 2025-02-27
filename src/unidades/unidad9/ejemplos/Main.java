0d3c6c18a2b8603ee381eef927458d368cec17b1
diurno <diurno@equipo-00>
2025-02-27 11:17:05 +0100

    vamoos

---
+++ src/unidades/unidad9/ejemplos/Main.java
@@ -0,0 +1,30 @@
+package unidades.unidad9.ejemplos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ILista listaPersona = new MiLista();
+		
+		Persona persona1 = new Persona(1, "Amparo");
+		Persona persona2 = new Persona(12, "Tomás");
+		Persona persona3 = new Persona(9, "Mbappé");
+		
+		
+		listaPersona.add(persona1);
+		listaPersona.add(persona2);
+		listaPersona.add(persona3);
+
+		listaPersona.mostrarLista();
+		
+		System.out.println("Numero de elementos: " + listaPersona.obtenerTamanio());
+		
+		if(listaPersona.searchPersona(persona3)) {
+			System.out.println(persona3 + " Persona encontrada");
+		}else {
+			System.out.println(persona3 + " Persona no encontrada");
+		}
+		
+	}
+
+}
