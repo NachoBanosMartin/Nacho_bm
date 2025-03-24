736af2cffac87b75401c0bc62469036b425d0dc4
diurno <diurno@equipo-00>
2025-03-24 10:20:07 +0100

    subo

---
+++ src/unidades/unidad9/ejemplos/metodosColeecciones/EscribirFicheroIO.java
@@ -0,0 +1,25 @@
+package unidades.unidad9.ejemplos.metodosColeecciones;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribirFicheroIO {
+
+	public static void main(String[] args) {
+
+		try {
+			BufferedWriter escritor = new BufferedWriter (new FileWriter("datos.txt", false));
+			
+			String datos = "Aquí vienen los datos";
+			
+			escritor.write(datos);
+			
+			escritor.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
